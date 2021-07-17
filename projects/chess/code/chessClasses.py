eb12c8c90fbfbcff209c776cb65ae7843aeaca95
Ryan O'Farrell <rofarrell.92@gmail.com>
2021-07-16 21:04:42 -0400

    Documentation, typing, clean up some redundant functions

--- projects/chess/code/chessClasses.py
+++ projects/chess/code/chessClasses.py
@@ -1,19 +1,36 @@
 # %% Imports
+from typing import List
 import numpy as np
 
 
 # %% Constants
-files = ["a", "b", "c", "d", "e", "f", "g", "h"]
-ranks = ["1", "2", "3", "4", "5", "6", "7", "8"]
+FILES = ["a", "b", "c", "d", "e", "f", "g", "h"]
+RANKS = ["1", "2", "3", "4", "5", "6", "7", "8"]
+SQUARES = [f + r for f in FILES for r in RANKS]
 
 
 class MoveError(Exception):
+    "Generic error for move errors"
     pass
 
 
-def getRelativeLoc(color, loc, addRank, addFile):
+def getRelativeLoc(color: str, loc: str, addRank: int, addFile: int) -> str:
     """Return the name of a cell that is the specified increment from loc.
     Always is from the perspective of the specified color, so black +1 rank is closer to 1
+
+    Args:
+
+        color (str) -- w or b
+
+        loc (str) -- location, like a1, g2, f8, etc.
+
+        addRank (int) -- how many ranks to increment
+
+        addFile (int) -- how many files to increment
+
+    Returns:
+
+        str -- initial location + specified ranks + files
     """
     if color == "b":
         mult = -1
@@ -27,14 +44,55 @@ def getRelativeLoc(color, loc, addRank, addFile):
     return relFile + relRank
 
 
-def getOtherColor(c):
+def getOtherColor(c: str) -> str:
+    "Returns the other color of w or b"
+    assert c in ["w", "b"], f"Invalid color {c}"
     if c == "w":
         return "b"
     return "w"
 
 
+def getLineFrom(
+    color: str, loc: str, rankIncrement: int, fileIncrement: int
+) -> List[str]:
+    """Returns an incremental line from specified location.
+
+    Args:
+
+        color (str) -- b or w
+
+        loc (str) -- a square like a1, g5, f8
+
+        rankIncrement (int) -- what to add each iteration to the rank
+
+        fileIncrement (int) -- what to add each iteration to the file
+
+    Returns:
+
+    list(str) -- the squares eminating in the specified vector from loc.
+
+    Example:
+
+    From a1, we can set a rankIncrement of 1 and a fileIncrement of 0 to get the a-file
+
+    From c3, we can set a rank and file increment of -1 to get ['b2', 'a1']
+
+    """
+    out = [
+        getRelativeLoc(
+            color, loc, addRank=(i + 1) * rankIncrement, addFile=(i + 1) * fileIncrement
+        )
+        for i in range(7)
+        if getRelativeLoc(
+            color, loc, addRank=(i + 1) * rankIncrement, addFile=(i + 1) * fileIncrement
+        )
+        in SQUARES
+    ]
+    return out
+
+
 def isInCheck(board: dict, color: str) -> bool:
-    """Given a board layout, returns bool if specified color is in check
+    """Given a board layout, returns if specified color is in check
 
     Args:
 
@@ -58,7 +116,7 @@ def isInCheck(board: dict, color: str) -> bool:
     pawnAttackSquares = [
         getRelativeLoc(color, k, 1, _)
         for _ in [1, -1]
-        if getRelativeLoc(color, k, 1, _) in board.keys()
+        if getRelativeLoc(color, k, 1, _) in SQUARES
     ]
     for s in pawnAttackSquares:
         if str(board[s]) == otherColor + "P":
@@ -70,14 +128,14 @@ def isInCheck(board: dict, color: str) -> bool:
         for r, f in zip([1, 2], [2, 1])
         for rankMult in [-1, 1]
         for fileMult in [-1, 1]
-        if getRelativeLoc(color, k, r * rankMult, f * fileMult) in board.keys()
+        if getRelativeLoc(color, k, r * rankMult, f * fileMult) in SQUARES
     ]
     for s in knightAttackSquares:
         if str(board[s]) == otherColor + "N":
             return True
 
     # Check that closest diagonal piece isn't bishop or Queen
-    diagonals = [getDiagonal(board, color, k, i, j) for i in [-1, 1] for j in [-1, 1]]
+    diagonals = [getLineFrom(color, k, i, j) for i in [-1, 1] for j in [-1, 1]]
     for diag in diagonals:
         for s in diag:
             if str(board[s]) == "  ":
@@ -87,7 +145,7 @@ def isInCheck(board: dict, color: str) -> bool:
 
     # Check that closest straight line piece isn't rook or Queen
     straights = [
-        getStraight(board, color, k, i * mult, j * mult)
+        getLineFrom(color, k, i * mult, j * mult)
         for i, j in zip([0, 1], [1, 0])
         for mult in [-1, 1]
     ]
@@ -99,30 +157,6 @@ def isInCheck(board: dict, color: str) -> bool:
                 return True
 
 
-def getDiagonal(board, color, loc, rankDir=1, fileDir=1):
-    out = [
-        getRelativeLoc(color, loc, addRank=(i + 1) * rankDir, addFile=(i + 1) * fileDir)
-        for i in range(7)
-        if getRelativeLoc(
-            color, loc, addRank=(i + 1) * rankDir, addFile=(i + 1) * fileDir
-        )
-        in board.keys()
-    ]
-    return out
-
-
-def getStraight(board, color, loc, rankDir=0, fileDir=1):
-    out = [
-        getRelativeLoc(color, loc, addRank=(i + 1) * rankDir, addFile=(i + 1) * fileDir)
-        for i in range(7)
-        if getRelativeLoc(
-            color, loc, addRank=(i + 1) * rankDir, addFile=(i + 1) * fileDir
-        )
-        in board.keys()
-    ]
-    return out
-
-
 # %% Chess game class
 class chessGame(object):
     "High-level object to store chess game state"
@@ -130,8 +164,8 @@ class chessGame(object):
     def __init__(self):
         # Create chess board, load up pieces
         self.board = {}
-        for f in files:
-            for r in ranks:
+        for f in FILES:
+            for r in RANKS:
                 self.board[f + r] = self.empty()
         for r, color in (["1", "w"], ["8", "b"]):
             for _ in ["a" + r, "h" + r]:
@@ -142,7 +176,7 @@ class chessGame(object):
                 self.board[_] = self.piece(color, "B")
             self.board["d" + r] = self.piece(color, "Q")
             self.board["e" + r] = self.piece(color, "K")
-            for f in files:
+            for f in FILES:
                 pawnRank = ["2", "7"][color == "b"]
                 self.board[f + pawnRank] = self.piece(color, "P")
 
@@ -158,33 +192,35 @@ class chessGame(object):
             self._toMove = "w"
             self._waiting = "b"
 
-    def move(self, old, new):
+    def _isPiece(self, itm):
+        "Is the itm a piece class?"
+        return type(itm) == self.piece
+
+    def move(self, old: str, new: str):
+        "Execute specified move from old to new"
 
         movingPiece = self.board[old]
+
+        # Ensure we are only moving pieces
+        if not self._isPiece(itm=movingPiece):
+            raise MoveError("Must move a piece")
+
         # Only move if its your turn
         if movingPiece.color != self._toMove:
             raise MoveError(f"It is {self._toMove} turn")
         print(f"Moving {movingPiece} from {old}->{new}")
+
         # Remove from from, place in to
         newBoard = self.board.copy()
         newBoard[new] = movingPiece
         newBoard[old] = self.empty()
 
         # Move is invalid if it results in moving color being in check
-        if isInCheck(newBoard, self._toMove):
+        if isInCheck(board=newBoard, color=self._toMove):
             raise MoveError(f"Move results in {self._toMove} in check")
 
         self.board = newBoard
         self._changeTurn()
-
-        # print(newBoard[getRelativeLoc(color, k, 1, 1)])
-        # print(newBoard[getRelativeLoc(color, k, 1, -1)])
-        # print(f"Looking for {self._waiting}P")
-        # if (newBoard[getRelativeLoc(color, k, 1, 1)] == self._waiting + "P") | (
-        #     newBoard[getRelativeLoc(color, k, 1, -1)] == self._waiting + "P"
-        # ):
-        #     print('In check!')
-        #     return True
 
     class piece(object):
         def __init__(self, color, name):
@@ -211,9 +247,9 @@ class chessGame(object):
         out = ""
         out += f"\n{self._toMove} to move"
         out += "\n  -----------------------------------------"
-        for r in [str(8 - x) for x in range(8)]:
+        for r in reversed(RANKS):
             out += f"\n{r} |"
-            for f in files:
+            for f in FILES:
                 out += f" {self.board[f+r]} |"
             out += "\n  -----------------------------------------"
         out += "\n    a    b    c    d    e    f    g    h"
@@ -225,7 +261,7 @@ class chessGame(object):
 if __name__ == "__main__":
     game = chessGame()
     # print(game)
-    game.move("e1", "h7")
+    game.move("e1", "e2")
 
     # game.move("g8", "g6")
     # game.move("e5", "e4")
